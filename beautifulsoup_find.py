bf85a02afc9c3fd6734ea761ab0a1428e092e3a3
sensival <alstjs883@naver.com>
2024-04-12 00:59:58 +0900

    rename

--- beautifulsoup_find.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.mirna.kr/Main/Index.nm"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content,"html.parser")
-
-print(soup.find_all("h5"))
